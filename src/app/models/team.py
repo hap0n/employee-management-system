cf1ed11ece962f7bea8c8f72d2909291cf87baa4
Maksym Hapon <maksym.hapon@proxet.com>
2021-09-13 05:31:28 +0300

    add some models

--- src/app/models/team.py
+++ src/app/models/team.py
@@ -1,10 +1,13 @@
+from typing import List, Optional
+
 from pydantic import BaseModel
+
 from app.models.employee import Employee
-from typing import List
 
 
 class Team(BaseModel):
     id: int
     name: str
     lead: Employee
-    employers: List[Employee]
+
+    employers: Optional[List[Employee]] = None
